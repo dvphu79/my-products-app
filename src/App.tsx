f44e30e9f85fed97b5d10503f4af3ba6aa77c05b
Phu DO <dvphu79@gmail.com>
2025-06-05 11:07:02 +0700

    run a web app with tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Feature from '@components/Feature';
+import Footer from '@components/Footer';
+import logo from '@images/logo.png';
 
-function App() {
-  const [count, setCount] = useState(0)
+const features = [
+  {
+    title: 'npm run start',
+    description: 'Run the React app in development mode with live reloading.',
+  },
+  {
+    title: 'npm run build',
+    description: 'Bundles the React app for deployment in production environment.',
+  },
+  {
+    title: 'npm run inline',
+    description: 'Inline all CSS and JS in a single minfied file.',
+  },
+];
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+const App = () => (
+  <div className='flex min-h-screen flex-col justify-center bg-gray-100 py-6 sm:py-12'>
+    <div className='relative py-3 sm:mx-auto sm:max-w-xl'>
+      <div className='to-light-blue-500 absolute inset-0 -skew-y-6 transform bg-gradient-to-r from-cyan-400 shadow-lg sm:-rotate-6 sm:skew-y-0 sm:rounded-3xl' />
+      <div className='relative bg-white px-4 py-10 shadow-lg sm:rounded-3xl sm:p-20'>
+        <div className='mx-auto max-w-md'>
+          <div>
+            <a href='https://digitalinspiration.com/'>
+              <img src={logo} className='h-7 sm:h-8' alt='Logo' />
+            </a>
+          </div>
+          <div className='divide-y divide-gray-200'>
+            <div className='space-y-5 py-8 text-base leading-6 text-gray-700 sm:text-lg sm:leading-7'>
+              <h1 className='text-lg font-semibold text-cyan-600'>
+                React and Tailwind CSS Starter Kit
+              </h1>
+              <p>Create a React project with Vite and Tailwind CSS.</p>
+              <div className='list-disc space-y-2'>
+                {features.map((feature) => (
+                  <Feature
+                    key={feature.title}
+                    title={feature.title}
+                    description={feature.description}
+                  />
+                ))}
+              </div>
+              <p className='text-sm font-medium text-cyan-500'>
+                Built with Tailwind CSS 4 and React 19.
+              </p>
+            </div>
+            <Footer />
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  </div>
+);
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcssPlugin from '@tailwindcss/vite';
+import viteConfigPaths from 'vite-tsconfig-paths';
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcssPlugin(),
+    viteConfigPaths(),
+  ],
 })
