d4462b255500c6efb394ddb9560e977627d30f91
Phu DO <dvphu79@gmail.com>
2025-06-06 10:02:55 +0700

    feat: Update Configure ESLint for TypeScript React project

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,8 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import pluginPrettier from 'eslint-plugin-prettier/recommended';
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -20,11 +19,7 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
-  },
-  pluginPrettier,
-)
+  }
+);
